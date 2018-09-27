d5e2ceeea363c1dca0ce5990f2d3c85cab0ef0f0
“Maxime <maximedenis2011@gmail.com>
2018-09-27 14:07:39 -0400

    Modification de Hello World

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+# Je suis un commentaire!
+
+echo "salut!"
+
+# exit
+# echo "On ne me verra jaamis =("
+
+# -n = Ne pas faire de retour de chariot
+echo -n "Vive "
+echo "Linux"
+
+# -e = Enable \n
+echo -e "\n2\n3\n"
+
+read -p "Quel âge avez-vous : " age
+echo $age "ans! Vous paraissez plus jeune!"
+
+echo -n "Question : "
+read resultat
+echo $resultat
