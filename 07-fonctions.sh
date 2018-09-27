bddab3d3fcc6c2fa75e0a92d2b3b3b039a16551c
“Maxime <maximedenis2011@gmail.com>
2018-09-27 15:21:36 -0400

    Modification des Fonctions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+maFonction()
+{
+	echo "paramètre : $1"
+}
+
+maFonction allo
+
+afficherInfo()
+{
+	echo "--------------------------------"
+	echo "Kernel : " `uname -rs`
+	echo "--------------------------------"
+}
+
+afficherInfo
