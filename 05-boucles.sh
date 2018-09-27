c38b77cc8d1ade3736bce9efff36249e525ee1e8
“Maxime <maximedenis2011@gmail.com>
2018-09-27 14:55:47 -0400

    Modification des Boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+# For loop
+
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls` # Accent grave (backtick) permets d'exécuter uen commande
+
+for element in $liste
+do
+	echo $element
+done
+
+user=`whoami`
+echo "Salut $user"
+
+# While loop
+
+resultat=o
+
+# -o = OR
+# -a = AND
+
+while test $resultat = o -o $resultat = a;do
+	read -p "Entrez une lettre : " resultat
+done
