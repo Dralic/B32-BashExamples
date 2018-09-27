b19dee39e597bc4306efe05b84c24a9a7b1e51df
“Maxime <maximedenis2011@gmail.com>
2018-09-27 16:52:43 -0400

    Modification d'exercice #2 et #3

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+afficherMenu()
+{
+	echo "----------------"
+	echo "d = dictionnaire"
+	echo "q = quitter"
+	echo "----------------"
+}
+
+lettre=a
+
+while [[ $lettre != d && $lettre != q ]];do
+	afficherMenu
+	read -p "Entrez une lettre : " lettre
+done
+
+if [[ $lettre == q ]];then
+	echo "Quitter le programme"
+	exit
+elif [[ $lettre == d ]];then
+	./exercice1.sh $1
+fi
--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+addSpace()
+{
+	for ((i=0;i<$1;i++))
+	do
+		echo -n " "
+	done
+}
+
+nbRepetitions=30
+
+for ((i=0;i<$nbRepetitions;i++))
+do
+	clear
+	addSpace $i
+	echo -n "@"
+	sleep 0.2
+done
+
+clear
+addSpace $nbRepetitions
+echo "BOOM"
